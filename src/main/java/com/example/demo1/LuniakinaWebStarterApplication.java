aeded6483319646be4bbc0d3588bc6ce93fd1f9c
valeriia.luniakina <valeriia.luniakina@pm.bet>
2022-02-14 20:03:48 +0200

    bootstrapped project

---
+++ src/main/java/com/example/demo1/LuniakinaWebStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuniakinaWebStarterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LuniakinaWebStarterApplication.class, args);
+  }
+
+}
